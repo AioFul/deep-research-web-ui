45b8386d6948375e27b86afa80c6367edbee2b72
Nuer <x@nuer.cc>
2025-12-19 16:47:27 +0800

    feat: Update GitHubButton component

--- app/components/GitHubButton.vue
+++ app/components/GitHubButton.vue
@@ -3,7 +3,7 @@
     color="primary"
     variant="subtle"
     icon="i-lucide-github"
-    to="https://github.com/AnotiaWang/deep-research-web-ui"
+    to="https://cnb.cool/aioful/deep-research"
     target="_blank"
   >
   </UButton>
